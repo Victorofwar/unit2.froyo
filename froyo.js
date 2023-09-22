94fa3100523a3bc57fa6cb274b7dfefb9b025381
Victor Santos <victorsantos@Itzels-iMac.lan>
2023-09-22 06:34:38 -0400

    completed up to level 5

---
+++ froyo.js
@@ -0,0 +1,12 @@
+let icecream = prompt ("please enter your favorite icecream");
+console.log (icecream);
+let flavorArray = icecream.split(",")
+console.log (flavorArray)
+
+for (let i =0; i < icecream.length; i++) {let flavors = iceream [i]
+let obj = {
+    flavor: i
+    
+};
+flavorArray.push(obj);}
+console.log (flavorArray);
